70af769cd754a50463927d6df9aee54afa4adf63
dongwoo <dwseo96@naver.com>
2024-08-19 23:58:00 +0900

    Initial commit

---
+++ src/main/java/com/eastwoo/socketkeyapi/SocketKeyApiApplication.java
@@ -0,0 +1,13 @@
+package com.eastwoo.socketkeyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketKeyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketKeyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eastwoo/socketkeyapi/SocketKeyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastwoo.socketkeyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketKeyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
